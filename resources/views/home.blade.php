24ec17badd3f913b5e4aef37d47ffaca0f800bd0
ValerioGc <valerio.gc1@gmail.com>
2022-09-28 18:06:22 +0200

    Avanzamento stilizzazione

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,11 @@
     <div class="container">
         <ul class="card-container">
             @foreach($movies as $card)
-                <li class="card">{{$card['title']}}</li>
+                <li class="card">
+                    <h3>{{$card['title']}}</h3>
+                    <p>{{$card['date']}}</p>
+                    <p>{{$card['vote']}}</p>
+                </li>
             @endforeach
         </ul>
     </div>
