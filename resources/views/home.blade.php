53957dccd7ce827063b635ca89670a825ba08025
ValerioGc <valerio.gc1@gmail.com>
2022-09-28 16:58:03 +0200

    Aggiunta stilizzazione

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,11 @@
 @section('page-title', 'Laravel Movies')
 
 @section('movies-container')
-    <ul class="card-container">
-        @foreach($movies as $card)
-            <li class="card">$card</li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <ul class="card-container">
+            @foreach($movies as $card)
+                <li class="card">{{$card['title']}}</li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
